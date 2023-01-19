9c65f4d2e394278e5f65252af44490374acb98ef
abdulhammed24 <abdlhamd3@gmail.com>
2023-01-19 21:32:36 +0100

    created some components

--- client/src/components/List.js
+++ client/src/components/List.js
@@ -2,9 +2,11 @@ import React from "react";
 import Card from "./Card";
 // import useFetch from "../../hooks/useFetch";
 
-const FeaturedProducts = ({ type }) => {
+const List = ({ subCats, maxPrice, sort, catId }) => {
   //   const { data, loading, error } = useFetch(
-  //     `/products?populate=*&[filters][type][$eq]=${type}`
+  //     `/products?populate=*&[filters][categories][id]=${catId}${subCats.map(
+  //       (item) => `&[filters][sub_categories][id][$eq]=${item}`
+  //     )}&[filters][price][$lte]=${maxPrice}&sort=price:${sort}`
   //   );
 
   const data = [
@@ -46,32 +48,18 @@ const FeaturedProducts = ({ type }) => {
   ];
 
   return (
-    <div className="my-[100px] mx-[200px]">
-      <div className="flex items-center justify-between mb-[50px]">
-        <h1 className="flex-[2] capitalize">{type} products</h1>
-        <p className="flex-[3] text-gray-500">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum
-          suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan
-          lacus vel facilisis labore et dolore magna aliqua. Quis ipsum
-          suspendisse ultrices gravida. Risus commodo viverra maecenas.
-        </p>
-      </div>
-      <div className="flex justify-center gap-[50px]">
-        {
-          //           error
-          //   ? "Something went wrong!"
-          //   : loading
-          //   ? "loading"
-          //               :
+    <div className="flex flex-wrap justify-between">
+      {
+        //       loading
+        // ? "loading"
+        //           :
 
-          data?.map((item) => (
-            <Card item={item} key={item.id} />
-          ))
-        }
-      </div>
+        data?.map((item) => (
+          <Card item={item} key={item.id} />
+        ))
+      }
     </div>
   );
 };
 
-export default FeaturedProducts;
+export default List;

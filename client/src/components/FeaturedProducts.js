007b22af829e6fb3acae3a919456eb6e110db408
abdulhammed24 <abdlhamd3@gmail.com>
2023-02-04 08:43:27 +0100

    made changes

--- client/src/components/FeaturedProducts.js
+++ client/src/components/FeaturedProducts.js
@@ -1,49 +1,9 @@
 import React from "react";
 import Card from "./Card";
-// import useFetch from "../../hooks/useFetch";
+import useFetch from "../hooks/useFetch";
 
 const FeaturedProducts = ({ type }) => {
-  //   const { data, loading, error } = useFetch(
-  //     `/products?populate=*&[filters][type][$eq]=${type}`
-  //   );
-
-  const data = [
-    {
-      id: 1,
-      img: "https://images.pexels.com/photos/818992/pexels-photo-818992.jpeg?auto=compress&cs=tinysrgb&w=1600",
-      img2: "https://images.pexels.com/photos/2036646/pexels-photo-2036646.jpeg?auto=compress&cs=tinysrgb&w=1600",
-      title: "Long Sleeve Graphic T-Shirt",
-      isNew: true,
-      oldPrice: 19,
-      newPrice: 12,
-    },
-
-    {
-      id: 2,
-      img: "https://images.pexels.com/photos/818992/pexels-photo-818992.jpeg?auto=compress&cs=tinysrgb&w=1600",
-      img2: "https://images.pexels.com/photos/2036646/pexels-photo-2036646.jpeg?auto=compress&cs=tinysrgb&w=1600",
-      title: " T-Shirt",
-      isNew: true,
-      oldPrice: 19,
-      newPrice: 12,
-    },
-
-    {
-      id: 3,
-      img: "https://images.pexels.com/photos/818992/pexels-photo-818992.jpeg?auto=compress&cs=tinysrgb&w=1600",
-      title: "Coat",
-      oldPrice: 19,
-      newPrice: 12,
-    },
-
-    {
-      id: 4,
-      img: "https://images.pexels.com/photos/818992/pexels-photo-818992.jpeg?auto=compress&cs=tinysrgb&w=1600",
-      title: "Short",
-      oldPrice: 19,
-      newPrice: 12,
-    },
-  ];
+  const { data, loading, error } = useFetch(`/products`);
 
   return (
     <div className="my-[100px] mx-[200px]">
@@ -58,17 +18,11 @@ const FeaturedProducts = ({ type }) => {
         </p>
       </div>
       <div className="flex justify-center gap-[50px]">
-        {
-          //           error
-          //   ? "Something went wrong!"
-          //   : loading
-          //   ? "loading"
-          //               :
-
-          data?.map((item) => (
-            <Card item={item} key={item.id} />
-          ))
-        }
+        {error
+          ? "Something went wrong!"
+          : loading
+          ? "loading"
+          : data?.map((item) => <Card item={item} key={item.id} />)}
       </div>
     </div>
   );
